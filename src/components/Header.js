4594cb1ee8d7072ccf3dae954b9065424d2ea77b
trina-bws <trina.abarquez@bluewolfsolutions.com>
2026-02-19 16:31:36 +0800

    Finish sign in page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 
 export default function Header () {
     return (
-        <header class="bg-white">
-            <nav aria-label="Global" class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
-                <div class="flex lg:flex-1">
-                    <span class="sr-only">My Apartment</span>
+        <header className="bg-white">
+            <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+                <div className="flex lg:flex-1">
+                    <span className="sr-only">My Apartment</span>
                 </div>
             </nav>
         </header>  
